298c021ea46278704a12f37d06ae317db62ae404
nikhilSettlemint <nikhil@settlemintindia.com>
2025-02-02 19:59:58 +0530

    fix:

--- src/index.tsx
+++ src/index.tsx
@@ -5,35 +5,36 @@
 
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 import * as config from "./Config";
 
 const msalInstanceProvider = new PublicClientApplication(generateMsalConfig());
 
 ReactDOM.render(
-<MsalProvider instance={msalInstanceProvider}>
-    <App/>
-</MsalProvider>,
-document.getElementById('root'));
+  <MsalProvider instance={msalInstanceProvider}>
+    <App />
+  </MsalProvider>,
+  document.getElementById("root")
+);
 
 // Refer https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-react#configure-your-javascript-spa
 function generateMsalConfig() {
-    const msalConfig = {
-        auth: {
-            clientId: config.clientId,
-            authority: config.authorityUrl,
-            redirectUri: "http://localhost:3000",
-        },
-        cache: {
-            cacheLocation: "sessionStorage", // This configures where your cache will be stored
-            storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-        }
-    };
+  const msalConfig = {
+    auth: {
+      clientId: config.clientId,
+      authority: config.authorityUrl,
+      redirectUri: "https://power-bi-dem.vercel.app",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+  };
 
-    return msalConfig;
+  return msalConfig;
 }
